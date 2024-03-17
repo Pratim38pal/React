cc1f841ac4667b4f886ed7f2da986e2304a87521
pratim pal <pratimpal38@gmail.com>
2024-03-17 21:03:00 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,25 @@
+import Chai from "./chai.jsx"
+import Chai from "./chai.js"
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <chai/>
+    <h1>Chai aur react</h1>
+    <p>test para</p>
+
+    
+    </>
+
+
+
+
+
+  )
+
+
+
+}
+export default App;
---
+++ react-for-youtube/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>hello khaise ho app sab
+      <p>lorem</p>
+      <div><h1>pratim is a genious</h1></div>
+    </div>
+  
+  
+  )
+}
+
+export default page
